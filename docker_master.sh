ffef297a692e86314885f109803d797457945a50
jeremypumphrey <jeremypumphrey@gmail.com>
2016-03-29 16:45:17 -0500

    revamped docker_master to use the individual scripts as they are written

--- docker_master.sh
+++ docker_master.sh
@@ -15,30 +15,26 @@ then
     exit 1
 fi
 
-echo 
-echo "Pulling images to be used"
-docker pull mongo:3.2.4
-docker pull ruby:2.2.4
-#docker pull matchbox/treatment-arm-api
-docker pull matchbox/tarm
-
 echo
-echo "Building new docker image for tarm based on local TA code"
-docker build -t matchbox/tarm ~/git/treatment-arm-api/treatment_arm_api/
-
 echo
-echo "Start DB Instance"
-docker run -d -v /tmp/mongodb -p 27017:27017 --name mongodb -e MONGODB_DBNAME=match mongo:3.2.4
-
+echo "----------Starting MongoDB--------------------------------------------------------------------"
+./docker_mongo.sh
 echo
-echo "Start TreatmentArmAPI Instance"
-#docker run -d --name TreatmentArm --link mongodb -p 3000:10235 matchbox/treatment-arm-api
-docker run -d --name TreatmentArm --link mongodb -p 3000:10235 matchbox/tarm
+echo
+echo "----------Starting RabbitMQ-------------------------------------------------------------------"
+./docker_rabbitmq.sh
+echo
+echo
+echo "----------Starting TreatmentArmAPI------------------------------------------------------------"
+./docker_treatmentarm.sh
 
+# TODO: Offer to load baseline data to DB
+echo
 echo
 echo "Loading TAs into DB"
 cd ~/git/treatment-arm-api/treatment_arm_api/scripts/treatment_arm_loader/app/
-ruby treatment_arm_loader.rb -u http://192.168.99.100:3000/treatmentarm/ -f ../spreadsheets/production/WAVE3_ARMS_FULL_LATEST.xlsx -s eay131_a
+ruby treatment_arm_loader.rb -u http://192.168.99.100:10235/ -f ../spreadsheets/production/WAVE3_ARMS_FULL_LATEST.xlsx -s eay131_a
+
 
 echo
 echo "Local Docker Machine IP"
